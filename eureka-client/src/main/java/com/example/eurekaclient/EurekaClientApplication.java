9909ed3fab4c94269d8926584ea2786ad6ccb100
Gan, Sze Kai <szekai.gan@sc.com>
2017-05-15 15:18:31 +0800

    eureka demo

--- eureka-client/src/main/java/com/example/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/example/eurekaclient/EurekaClientApplication.java
@@ -1,18 +1,14 @@
-package com.example.admin;
+package com.example.eurekaclient;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@EnableAdminServer
 @EnableDiscoveryClient
-public class 	AdminApplication {
+public class EurekaClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AdminApplication.class, args);
+		SpringApplication.run(EurekaClientApplication.class, args);
 	}
 }
