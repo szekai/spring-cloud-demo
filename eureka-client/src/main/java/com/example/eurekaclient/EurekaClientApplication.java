f5f92092c776d4d370f3a06c8eba1b4a63cf7d21
Gan, Sze Kai <szekai.gan@sc.com>
2017-05-16 10:20:55 +0800

    update Eureka Client

--- eureka-client/src/main/java/com/example/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/example/eurekaclient/EurekaClientApplication.java
@@ -1,14 +1,24 @@
 package com.example.eurekaclient;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableAutoConfiguration
+@RestController
 public class EurekaClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello world";
 	}
 }
